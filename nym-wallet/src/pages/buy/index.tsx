62a1ead052f1ffca9e1471bc6521de14f777b8af
pierre <dommerc.pierre@gmail.com>
2022-09-14 19:40:59 +0200

    feat(wallet): add sign message modal component

--- nym-wallet/src/pages/buy/index.tsx
+++ nym-wallet/src/pages/buy/index.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import { LoadingModal } from 'src/components/Modals/LoadingModal';
-import { BuyContextProvider, useBuyContext } from 'src/context';
+import { BuyContextProvider } from 'src/context';
 import { Tutorial } from 'src/components/Buy/Tutorial';
-
-const Buy = () => {
-  const { isLoading } = useBuyContext();
-  if (isLoading) {
-    return <LoadingModal />;
-  }
-
-  return <Tutorial />;
-};
 
 export const BuyPage = () => (
   <BuyContextProvider>
-    <Buy />
+    <Tutorial />
   </BuyContextProvider>
 );
